4fa7dd3353253d8e003bf30a91f971881196eca4
rishisingh34 <singh34rishi@gmail.com>
2024-09-09 09:09:59 +0530

    chore: Update dependencies in package.json and package-lock.json

--- controllers/auth.controller.ts
+++ controllers/auth.controller.ts
@@ -1,6 +1,5 @@
 import StudentModel from "../models/student.model";
 import { Request, Response } from 'express';
-import axios from "axios";
 import {RECAPTCHA_SECRET_KEY} from '../config/env.config';
 import Activity from "../models/activity.model";
 // import Token from '../middleware/token.middleware';
@@ -8,23 +7,14 @@ import Activity from "../models/activity.model";
 const authController={
     login: async(req:Request,res:Response):Promise<Response>=>{
         try{
-            const {studentNumber,password,token}=req.body;
-            // const {data} = await axios.post(`https://www.google.com/recaptcha/api/siteverify?secret=${RECAPTCHA_SECRET_KEY}&response=${token}`);
-            // if(!data.success || data.score < 0.5){
-            //     return res.status(400).json({error:"Invalid Captcha"});
-            // }
+            const {studentNumber,password}=req.body;
             const student=await StudentModel.findOne({studentNumber,password});
             if(!student)
             {
                 return res.status(400).json({message:"Invalid credentials"});
             }
             await Activity.findOneAndUpdate({userId:student.id},{lastActivity:Date.now()});
-            // const accessToken=await Token.signAccessToken(student.id);
-            // res.cookie("accessToken", accessToken, {
-            //     httpOnly: true,
-            //     secure: true,
-            //     sameSite: "none",
-            // });
+            
             return res.status(200).json({message:"Login successful", userId : student.id  });
         }
         catch(error)
